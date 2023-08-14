b3723a9978e60daab7bf810d7ac5cb5b95765294
Maschkov Konstantin <maschkovc@yandex.ru>
2023-08-14 17:16:02 +0300

    fetch

--- js/script.js
+++ js/script.js
@@ -259,7 +259,7 @@ document.addEventListener('DOMContentLoaded', () => {
             e.preventDefault();
 
             const statusMessage = document.createElement('img');
-            statusMessage.src  = message.loading;
+            statusMessage.src = message.loading;
             statusMessage.style.cssText = `
                 display: block;
                 margin: 0 auto;
@@ -267,10 +267,6 @@ document.addEventListener('DOMContentLoaded', () => {
             // form.append(statusMessage);
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
-            request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
 
             const object = {};
@@ -278,21 +274,21 @@ document.addEventListener('DOMContentLoaded', () => {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
-            request.send(json);
 
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    // console.log(request.response);
-                    showThanksModal(message.success);
-                  
-
-                    statusMessage.remove();
-                    form.reset();
-
-                } else {
-                    showThanksModal(message.failure);
-                }
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object),
+            }).then(data => data.text()).then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             });
         });
     }
